2dd8b4ea446cd8a9c5a05ff9e9e9d108cbbe54d0
User-PC\User <13439017540@163.com>
2018-02-02 21:04:20 +0800

    no message

---
+++ python/第一周/hello.py
@@ -0,0 +1,3 @@
+# -*- coding:utf-8 -*-
+# A uthor: davie
+print("Hello, world!")
--- python/第一周/第04章节-Python3.5-开课介绍.py
+++
@@ -1,7 +0,0 @@
-# _*_ coding:utf-8 _*_
-'''
-你花钱来学PYTHON的原因
-1、节省时间，投资自己
-2、结识更多的朋友
-3、Become a better version of yourself
-'''
