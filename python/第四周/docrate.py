20511c312dec2c3737e3ff944d23574952bb3f3e
User-PC\User <13439017540@163.com>
2018-03-10 06:51:37 +0800

    第一模块作业

---
+++ python/第四周/docrate.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Author: davie
+"""
+装饰器：
+定义：本质是函数，(装饰其他函数)，就是为其他函数添加附加功能
+原则：1、不能修改被装饰的函数的源代码；
+      2、不能修改被装饰的函数的调用方式；
+      
+实现装饰器只是储备：
+1、函数即"变量"
+2、高阶函数
+3、嵌套函数
+"""
+import time
+def timmer(func):
+	def warpper(*args,**kwargs):
+		start_time=time.time()
+		func()
+		stop_time=time.time()
+		print("the func tun time is %s" %(stop_time-start_time))
+	return warpper
+@timmer
+def test1():
+	time.sleep(3)
+	print('in the test1')
+test1()
+
